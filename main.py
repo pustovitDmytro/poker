0d3ea268af8be439a47ac9380196547e0e1d1369
dmytroPustovit <pustic@gmail.com>
2017-07-01 16:44:49 +0300

    basic functions added

--- main.py
+++ main.py
@@ -22,9 +22,50 @@ def fit(comb):
     else:
         return False
 
+def getLineFromFile(way,pos=0):
+    f = open(way,'rU')
+    f.seek(pos)
+    line = f.readline()
+    nextPos = f.tell()
+    if not line: nextPos = -1
+    f.close()
+    return(nextPos,line)
+def putLineToFile(way,line):
+    f = open(way,'a')
+    f.writelines(line)
+    f.close()
+def clearFile(way):
+    f = open("out.txt", 'w')
+    f.truncate(0)
+    f.close()
 
 
+def strToCard(str,position):
+    obj = {
+        'face':str[0],
+        'suit':str[1],
+        'position':position
+    }
+    return obj
 
-
-
-fit([1,5,8,9,4])
+position=0
+clearFile("out.txt")
+while True:
+    (position,line) = getLineFromFile("test.txt",position)
+    if position<0:
+        break
+    strCards = line.replace('\n','').split(' ')
+    cards = []
+    for i,strCard in enumerate(strCards):
+        cards.append(strToCard(strCard,i))
+    print(cards)
+    outline = "Hand: "
+    for i in range(5):
+        outline+=strCards[i]+" "
+    outline+="Deck: "
+    for i in range(5,10):
+        outline+=strCards[i]+" "
+    outline+="Best hand: "
+    outline+='\n'
+    print(outline)
+    putLineToFile("out.txt",outline)
